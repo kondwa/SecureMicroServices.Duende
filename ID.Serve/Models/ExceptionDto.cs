0744125698556ddbb46e57b34427c43c79556c12
Kondwani Hara <kondwa@gmail.com>
2025-05-04 16:44:49 +0200

    Make ExceptionDto properties nullable and add defaults
    The `Id` property in the `ExceptionDto` class was updated to be nullable (`string?`) instead of non-nullable (`string`).
    Default values (`string.Empty`) were added to the `Message`, `StackTrace`, and `Source` properties to ensure they are always initialized.
    These properties were previously non-nullable but lacked default values.

--- ID.Serve/Models/ExceptionDto.cs
+++ ID.Serve/Models/ExceptionDto.cs
@@ -2,10 +2,10 @@
 {
     public class ExceptionDto
     {
-        public string Id { get; set; }
+        public string? Id { get; set; }
         public int StatusCode { get; set; }
-        public string Message { get; set; }
-        public string StackTrace { get; set; }
-        public string Source { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string StackTrace { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
     }
 }
